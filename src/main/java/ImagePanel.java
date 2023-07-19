0a6677f8cc91ca95c98d8676d8f6133f4a4cec54
Stefan552 <lacokovacs93@gmail.com>
2023-07-19 23:40:24 +0300

    KeyManager1

--- src/main/java/ImagePanel.java
+++ src/main/java/ImagePanel.java
@@ -1,24 +1,16 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
 
-class ImagePanel extends JPanel {
+public class ImagePanel extends JPanel {
     private Image backgroundImage;
 
-    public ImagePanel ( String imagePath) {
-        try {
-            backgroundImage = ImageIO.read(getClass().getResource(imagePath));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public ImagePanel(Image backgroundImage) {
+        this.backgroundImage = backgroundImage;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (backgroundImage != null) {
-            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
-        }
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
     }
 }
